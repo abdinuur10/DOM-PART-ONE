8608146110b227ef4ec595c9c7fe887905efd7d5
ABDIRASHID NUUR <78458279+abdinuur10@users.noreply.github.com>
2021-02-27 00:14:02 -0800

    Done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,80 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// navigation
+
+let navitems =document.querySelectorAll("nav a")
+console.log(navitems)
+
+navitems.forEach(function(item, index) {
+  item.textContent = siteContent.nav[`nav-item-${index +1}`]
+})
+
+// site contacts.
+
+const nav = document.getElementsByClassName("container")
+console.log(nav)
+
+let newCitation = document.querySelector("cta-text")
+console.log(newCitation)
+
+let newTitle = Document.querySelector(".cta-text")
+
+newTitle.textContent = "DOM Is Awesome"
+newTitle.textContent = "Get Started"
+
+let logo = document.getElementById("header-img");
+logo.setAttribute('src', siteContent["cta"]["img-src"])
+
+// Main Content.
+
+let changeMaintextP= document.querySelector(".text-contact")
+changeMaintextP.textContent = siteContent["main-content"]["features-content"]
+
+let changeMaintextP= document.querySelector(".text-contact h4")
+changeMaintextP.textContent = siteContent["main-content"]["features-content h4"]
+
+const changeMaintextP= document.querySelector(".text-contact")
+changeMaintextP.textContent = siteContent["main-content"]["About-content"]
+
+const changeMaintextP= document.querySelector(".text-contact h4")
+changeMaintextP.textContent = siteContent["main-content"]["About-content h4"]
+
+const changeMaintextP= document.querySelector(".text-contact")
+changeMaintextP.textContent = siteContent["main-content"]["Service-content"]
+
+const changeMaintextP= document.querySelector(".text-contact h4")
+changeMaintextP.textContent = siteContent["main-content"]["Service-content h4"]
+
+const changeMaintextP= document.querySelector(".text-contact")
+changeMaintextP.textContent = siteContent["main-content"]["Product-content"]
+
+const changeMaintextP= document.querySelector(".text-contact h4")
+changeMaintextP.textContent = siteContent["main-content"]["Product-content h4"]
+
+const changeMaintextP= document.querySelector(".text-contact")
+changeMaintextP.textContent = siteContent["main-content"]["Vision-content"]
+
+let changeMaintextP= document.querySelector(".text-contact h4")
+changeMaintextP.textContent = siteContent["main-content"]["Vision-content h4"]
+
+// middle image;
+let changemidpage = document.querySelector(".middle-img")
+
+changemidpage.setAttribute ('src', siteContent["middle-img"]['image-src'])
+
+// Contact:
+
+let changeMaintext = document.querySelector(".Contact")
+
+changeMaintext.textContent = "address 123 Way 456 Street Somewhere, USA",
+changeMaintext.textContent = "phone 1 (888) 888-8888",
+changeMaintext.textContent = "email: sales@greatidea.io"
+
+// Footer
+
+const newContent = document.createElement("Footer")
+console.log(newContent)
+
+newContent.textContent = "Copyright Great Idea! 2018"
